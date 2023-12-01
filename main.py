ac8921c44623a5a9b64839c64bb861d485027fec
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-12-01 16:25:55 +0700

    fix: cors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.brand import brand
 from routes.shoes import shoes
@@ -7,6 +7,18 @@ from routes.varian import varian
 from routes.auth import auth
 
 app = FastAPI()
+
+# Setelan CORS untuk menerima permintaan dari semua domain
+origins = ["*"]
+
+# Tambahkan middleware CORS ke aplikasi
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(brand)
 app.include_router(shoes)
